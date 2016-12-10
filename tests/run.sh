a6361303346f80ee7727afa29334ea887240102a
Cheng Lou <chenglou92@gmail.com>
2016-12-09 22:18:53 -0800

    Make tests pass again

--- tests/run.sh
+++ tests/run.sh
@@ -2,16 +2,16 @@ set -e
 
 esc=$(printf '\033')
 checkmark="${esc}[32m✔${esc}[0m";
-crossmark="${esc}[31m✔${esc}[0m";
+crossmark="${esc}[31m✘${esc}[0m";
 
 thisDir=$(dirname "$0")
-expected=`cat $thisDir/fixture.js`
+expected=`cat $thisDir/expected.js`
 actual=`cat $thisDir/test.js`
 
 if [[ "$expected" = "$actual" ]]; then
-  echo "$checkmark  test.js looks like the expected output (fixture.js)."
+  echo "$checkmark test.js looks like the expected output (expected.js)."
 else
-  echo "$crossmark  Wrong"
-  diff -u $thisDir/fixture.js $thisDir/test.js
+  echo "$crossmark Wrong"
+  diff -u $thisDir/expected.js $thisDir/test.js
   exit 1
 fi
--- tests/test.re
+++ tests/test.re
@@ -2,7 +2,7 @@ open ReasonJs;
 
 let date = Date.now ();
 
-let myInterval = setInterval (fun () => Console.log "hello!") 1000;
+let myInterval = setInterval (fun () => Js.log "hello!") 1000;
 
 let width = Window.(innerWidth window);
 
@@ -11,4 +11,3 @@ let heigth = Window.(innerHeight window);
 Window.(addEventListener window "click" (fun () => print_endline "asd"));
 
 Window.(onLoad window (fun () => print_endline "load"));
-
