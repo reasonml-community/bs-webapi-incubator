491e36c824c53c7a3f76902b20ddda0d6d73cd02
Cheng Lou <chenglou92@gmail.com>
2017-03-20 14:58:33 -0700

    bspack script

--- convert_to_ml.sh
+++ convert_to_ml.sh
@@ -7,3 +7,19 @@ for i in $(find src/**/*.re); do
 done
 
 node ./sed.js `find src/**/*.ml`
+
+./node_modules/bs-platform/bin/bspack.exe \
+-I src/storage \
+-I src/dom \
+-I src/dom/css \
+-I src/dom/events \
+-I src/dom/html \
+-I src/dom/nodes \
+-I src/dom/ranges \
+-I src/dom/selection \
+-I src/dom/sets \
+-I src/dom/traversal \
+-bs-main DomRe \
+-o test_dom.ml
+
+./node_modules/bs-platform/bin/bsc.exe test_dom.ml
